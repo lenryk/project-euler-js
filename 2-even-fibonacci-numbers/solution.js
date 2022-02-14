922e17df6a46685edfca15f74cd154a8732b7591
lenryk <lenry101@gmail.com>
2022-02-14 17:38:18 +0000

    solution for problem 2

---
+++ 2-even-fibonacci-numbers/solution.js
@@ -0,0 +1,16 @@
+function calcFibs(maxLimit) {
+  let fibs = [0, 1];
+  while (fibs[fibs.length - 1] < maxLimit) {
+    fibs.push(
+      fibs
+        .slice(fibs.length - 2, fibs.length)
+        .reduce((accum, current) => accum + current)
+    );
+  }
+
+  return fibs
+    .filter((num) => num % 2 == 0)
+    .reduce((accum, current) => accum + current);
+}
+
+console.log(calcFibs(4_000_000));
