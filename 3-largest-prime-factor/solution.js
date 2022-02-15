4dc868d54c09dd571a1040a9099f630965e367f8
lenryk <lenry101@gmail.com>
2022-02-15 18:38:49 +0000

    solution for problem 3

---
+++ 3-largest-prime-factor/solution.js
@@ -0,0 +1,21 @@
+let number = 600851475143;
+const factors = []
+let finished = false
+let divide = 2
+
+while(!finished) {
+    if (number % divide !== 0) {
+        if (divide < number) {
+            divide++
+        } else {
+            finished = true
+        }
+        
+    } else {
+        factors.push(divide)
+        number = number / divide
+        divide = 2
+    }
+}
+
+console.log(factors.sort((a,b) => a - b)[factors.length - 1])
