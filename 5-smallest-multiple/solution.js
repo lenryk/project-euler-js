c9906b6b1a57a0d165b679a3731085414e3b3a67
lenryk <lenry101@gmail.com>
2022-02-17 17:39:59 +0000

    solution to problem 5

---
+++ 5-smallest-multiple/solution.js
@@ -0,0 +1,15 @@
+let finished = false
+let i = 1
+
+while(!finished) {
+    let counter = 0
+    let largestNum = 20
+    for (let n = 1; n < largestNum + 1; n++) {
+        if (i % n === 0) counter++
+        if (counter === largestNum) {
+            console.log(i)
+            finished = true
+        }
+    }
+    i++
+}
