94ba913e1ceaa61a9dcf0ff8aa89cb46b65b84bd
lenryk <lenry101@gmail.com>
2022-02-19 22:04:06 +0000

    solution to problem 7

---
+++ 7-10001st-prime/solution.js
@@ -0,0 +1,27 @@
+function findNPrimeNumber(n) {
+    let counter = 2
+    let number = 3
+
+    while(n + 1 !== counter) {
+        if (checkPrime(number) <= 2) counter++
+        number++
+    }
+
+    return number - 1
+}
+
+function checkPrime(number) {
+    let counter = 1
+
+    for (let prime = 2; prime < number + 1; prime++) {
+        if (number % prime === 0) {
+            counter++
+        } else if (counter > 2) {
+            break
+        }
+    }
+
+    return counter
+}
+
+console.log(findNPrimeNumber(10_001))
